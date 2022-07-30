b6b569e3501c0dfb4129ae8f4825c5f4c4b75f17
BrunoAngelis <bng.8@outlookcom>
2022-07-30 17:14:19 -0300

    Adicionando projeto

---
+++ src/ContaBanco.java
@@ -0,0 +1,9 @@
+public class ContaBanco {
+    public static void main(String[] args) {
+
+        ContaTerminal cliente = new ContaTerminal();
+
+        cliente.CadastrarCliente();
+        cliente.ExibirDados();
+    }
+}
---
+++ src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    String nomeCliente;
+    String agencia;
+    int numero;
+    double saldo = 0.00;
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void CadastrarCliente(){
+
+        System.out.printf("Digite o nome para cadastro: ");
+        nomeCliente = scanner.next();
+        System.out.printf("Digite a agência: ");
+        agencia = scanner.next();
+        System.out.printf("Digite o numero da conta : ");
+        numero = scanner.nextInt();
+        System.out.println("Digite o valor do primeio deposito em conta: ");
+        saldo = scanner.nextDouble();
+    }
+
+    public void ExibirDados(){
+        System.out.println("Olá " +nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque.");
+    }
+
+}
